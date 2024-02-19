d904ae8a9c15f7df8e08a772e9c2f0ab721a1b9d
Javier Gonzalez <javierggt@yahoo.com>
2024-02-19 16:02:30 -0500

    args

--- action/main.py
+++ action/main.py
@@ -1,6 +1,7 @@
-import pprint
 import json
 import yaml
+import logging
+import sys
 
 
 def main():
@@ -8,8 +9,37 @@ def main():
         arguments = json.load(fh)
     with open("argument_spec.yml") as fh:
         argument_spec = yaml.load(fh, Loader=yaml.SafeLoader)
-    pprint.pprint(arguments)
-    pprint.pprint(argument_spec)
+        argument_spec = {arg["name"]: arg for arg in argument_spec}
+
+    # Check for unknown or missing arguments
+    missing = [
+        name
+        for name in argument_spec
+        if argument_spec[name].get("required", False) and name not in arguments
+    ]
+    unknown = [name for name in arguments if name not in argument_spec]
+    if missing or unknown:
+        if missing:
+            logging.error("Missing arguments:", missing)
+        if unknown:
+            logging.error("Unknown arguments:", unknown)
+        raise sys.exit(1)
+
+    # if not required and default is not specified, then default is the empty string
+    arguments = {
+        name: arguments.get(name, argument_spec[name].get("default", ""))
+        for name in argument_spec
+    }
+
+    # if type is bool, coerce into string with values 'true' or 'false'
+    for name in argument_spec:
+        if argument_spec[name].get("type", "str") == "bool":
+            arguments[name] = (
+                "true" if str(arguments[name]) in ["true", "True", "1"] else "false"
+            )
+
+    for key, value in arguments.items():
+        print(f'{key}="{value}"')
 
 
 if __name__ == "__main__":

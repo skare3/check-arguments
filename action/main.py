5507cf4769fa624460eba0acc6aad69e5995d884
Javier Gonzalez <javierggt@yahoo.com>
2024-07-03 14:38:48 -0400

    write args separately

--- action/main.py
+++ action/main.py
@@ -44,6 +44,8 @@ def main():
     
     with open(os.environ["GITHUB_OUTPUT"], "r+") as fh:
         fh.write(f'arguments={json.dumps(arguments)}\n')
+        for key, value in arguments.items():
+            fh.write(f'{key}="{json.dumps(value)}"\n')
 
 
 if __name__ == "__main__":
